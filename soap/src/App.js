9a7bec0bdf92320fdd4c4991263ef5a8d1404d29
Laura Augusta <laurah.algusta@gmail.com>
2023-03-08 01:41:31 -0300

    bootstrap

--- soap/src/App.js
+++ soap/src/App.js
@@ -2,12 +2,14 @@ import './styles/App.scss';
 import './styles/index.css';
 import EstiloGlobal from './styles/styles.jsx';
 import Navbar from './components/Navbar';
+import ContainerEx from './components/Colu';
 
 function App() {
   return (
     <div className="App">
       <EstiloGlobal/>
       <Navbar />
+      <ContainerEx />
       <header className="App-header">
         <h1 className='bg-red-300'>TODO</h1>
       </header>
